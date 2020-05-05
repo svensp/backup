30c05e460149e0297cd2b70e2fb4ff57abfd458a
Sven Speckmaier <sps@ipunkt.biz>
2020-05-05 15:38:31 +0200

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM bitnami/minideb:buster
+
+RUN apt-get update \
+   && apt-get -y install \
+      python3 \
+      borgbackup \
+   && pip3 install paramiko-ng \
+   && rm -Rf /var/lib/apt/lists/*
+
+ENV PATH /opt/cloudbackup:$PATH
+
+COPY . /opt/cloudbackup
+
+ENTRYPOINT /opt/cloudbackup/cloudbackup
---
+++ backup/commands/files_prune_command.py
@@ -0,0 +1,39 @@
+from .command import Command
+from datetime import datetime
+import subprocess
+
+class FilesPruneCommand(Command):
+    def __init__(self):
+        self._name = "files:prune"
+        self._description('Mount a backup to restore files from it.')
+
+    def setBorg(self, borg):
+        self._borg = borg
+        return self
+
+    def run(self, parameters):
+        if len(parameters) < 3:
+            self.printHelp()
+            return 1
+
+        resortName = parameters[0]
+        resort = self._storage.findResort(resortName)
+
+        name = parameters[1]
+        target = parameters[2]
+
+        repositoryNumber = 1
+        if len(parameters) >= 4:
+            repositoryNumber = parameters[3]
+            
+        resort.passAdapters(self)
+        self._borg.mount(name, target, repositoryNumber)
+
+    def printHelp(self):
+        print("Usage:")
+        print(self._name+" RESORTNAME BACKUP TARGET [REPOSITORY]\n")
+        print("Create a backup")
+        print("- RESORTNAME: The resort in which to make file backup available")
+        print("- BACKUP: The name of the backup. Check available bakcups with files:list")
+        print("- TARGET: Directory to mount the backup to")
+        print("- REPOSITORY: From which repository to mount. Defaults to 1")
